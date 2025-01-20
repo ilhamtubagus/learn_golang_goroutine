2ce26f55d724db2035e576fa70d41ce58ecd65d2
ilhamarfian <ilham.arfian@btpn.com>
2025-01-20 19:35:29 +0700

    goroutine read and write mutex

--- mutex_test.go
+++ mutex_test.go
@@ -25,3 +25,37 @@ func TestRaceConditionWithMutex(t *testing.T) {
 	// Final value of x is 100,000. Race conditions are avoided using a mutex.
 	fmt.Println("Final value of x:", x)
 }
+
+type BankAccount struct {
+	RWMutex sync.RWMutex
+	Balance int
+}
+
+func (ba *BankAccount) AddBalance(amount int) {
+	ba.RWMutex.Lock()
+	defer ba.RWMutex.Unlock()
+
+	ba.Balance += amount
+}
+
+func (ba *BankAccount) GetBalance() int {
+	ba.RWMutex.RLock()
+	defer ba.RWMutex.RUnlock()
+
+	return ba.Balance
+}
+
+func TestRWMutexBankAccount(t *testing.T) {
+	account := BankAccount{}
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
+				account.AddBalance(1)
+				fmt.Println("Balance:", account.GetBalance())
+			}
+		}()
+	}
+
+	time.Sleep(3 * time.Second)
+}
