d5d652c9f81b54f712803ce24ca8df69e9fc2e7e
ilhamarfian <ilham.arfian@btpn.com>
2025-01-20 18:41:31 +0700

    goroutine select channel

--- channel_test.go
+++ channel_test.go
@@ -78,3 +78,47 @@ func TestBufferedChannel(t *testing.T) {
 	time.Sleep(2 * time.Second)
 	fmt.Println("Done")
 }
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			channel <- fmt.Sprintf("Message %d", i)
+		}
+		defer close(channel)
+	}()
+
+	for message := range channel {
+		fmt.Println(message)
+	}
+
+	fmt.Println("Done")
+}
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Received data from channel1:", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Received data from channel2:", data)
+			counter++
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+
+}
